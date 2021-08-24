505aaf6fcd68a6256ad3210a4ed013692d8c1071
Sangmin <23mean@naver.com>
2021-08-24 17:58:04 +0900

    2021.08.24
    integer_descending 풀이 완

--- Programmers/Level1/test.js
+++ Programmers/Level1/test.js
@@ -1,9 +1,1 @@
 //test page
-let tmp = ["1", "2", "3", "4", "5"];
-let tmp2 = [1, 2, 3, 4, 5];
-
-for (let i = 0; i < tmp.length; i++)
-    tmp[i] = Number(tmp[i]);
-
-console.log(tmp);
-console.log(tmp2);
