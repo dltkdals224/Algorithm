5c17094d21ee37eb511a68d26b638c50037e069d
sangmin <23mean@naver.com>
2021-08-25 15:42:13 +0900

    2021.08.25
    시저 암호, 약수의 합 풀이 추가.

---
+++ Programmers/Level1/sum_of_factors.js
@@ -0,0 +1,18 @@
+//2021.08.25
+function solution(n) {
+    let answer = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (n % i == 0)
+            answer += i;
+    }
+    return answer;
+}
+
+
+//다른 풀이
+/*
+function solution(n, a=0, b=0) {
+    return n<=a/2?b:solution(n,a+1,b+=n%a?0:a);
+}
+*/
