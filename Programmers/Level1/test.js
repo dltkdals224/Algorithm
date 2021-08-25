12b70df11f8545ca44f4fbe21063e5cfb7ed823c
sangmin <23mean@naver.com>
2021-08-25 15:42:13 +0900

    2021.08.25
    시저 암호, 약수의 합 풀이 추가.

--- Programmers/Level1/test.js
+++ Programmers/Level1/test.js
@@ -1,25 +1,5 @@
-//test page
-string = "try hello world";
-let ans = '';
+var val = "charCodeAt";
 
-console.log(string);
+ 
 
-let arr = string.split(' ', string.length);
-console.log(arr);
-let arr2 = '';
-
-for (let i = 0; i < arr.length; i++)
-    for (let j = 0; j < arr[i].length; j++) {
-        arr2 = arr[i].split('', arr[i].length);
-    }
-
-console.log(arr2);
-
-//console.log(ans);
-//let arr2 = arr.split('', arr.length);
-
-//.toUpperCase()
-//.toLowerCase()
-
-//.join()
-//.toString()
+console.log("val.charCodeAt(4) : " + val.charCodeAt(3) + "<br>");
