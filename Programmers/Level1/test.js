5d9b680d9810ed092591347caca4f22303554e29
Sangmin <23mean@naver.com>
2021-09-23 22:02:46 +0900

    //2021.09.23
    너무 어려워서 쉬운거 먼저 풉니다

--- Programmers/Level1/test.js
+++ Programmers/Level1/test.js
@@ -1,2 +1,27 @@
-let cor = [1, 2, 3, 4, 5, 5, 5];
-console.log(Math.max(1, 2, 2, 3, 3));
+import React, { useState } from "react";
+export default function Counter() {
+  const [state, setState] = useState({ count: 0 });
+  console.log("mounted or updated");
+  return (
+    <div>
+      {" "}
+      <p>{state.count}</p>{" "}
+      <button
+        onClick={() => {
+          setState((prevState) => ({ count: prevState.count + 1 }));
+        }}
+      >
+        {" "}
+        +1{" "}
+      </button>{" "}
+      <button
+        onClick={() =>
+          setState((prevState) => ({ count: prevState.count - 1 }))
+        }
+      >
+        {" "}
+        -1{" "}
+      </button>{" "}
+    </div>
+  );
+}
