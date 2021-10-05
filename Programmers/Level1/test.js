c45ff81b444821cfb8dd12160f65df21b026bbe6
Sangmin <23mean@naver.com>
2021-10-05 13:51:15 +0900

    2021.10.05
    풀이 완

--- Programmers/Level1/test.js
+++ Programmers/Level1/test.js
@@ -1,7 +1,2 @@
-const arr = ["1", "2", "3"];
-let sangmin = new Set(arr);
-
-console.log(sangmin); //Set(3) {'1','2','3'}
-console.log(sangmin.size); //3
-console.log(Object.keys(sangmin).length); //0
-//key값이 따로 존재하지 않기 때문.
+new C();
+class C {}
