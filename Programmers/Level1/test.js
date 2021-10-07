9d401fdb4475f18a03b6800af182171942b24826
Sangmin <23mean@naver.com>
2021-10-07 12:03:54 +0900

    2021.10.07
    kakao lotto 완

--- Programmers/Level1/test.js
+++ Programmers/Level1/test.js
@@ -1,19 +1,16 @@
-function p() {
+// Promise.race([프로미스 객체들]);
+
+function p(ms) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      //js에서 사용하는 error객체
-      reject(new Error("Fatal Error"));
-    }, 1000);
+      resolve(ms);
+    }, ms);
   });
 }
 
-p()
-  .then((message) => {
-    //resolve를 통해 매개변수가 전달됨
-    console.log("1000ms후에 fulfilled됩니다.", message);
-  })
-  //catch : 비동기 함수가 잘못되었을때의 상태를 잡는 code
-  .catch((reason) => {
-    console.log("1000ms후에 rejected됩니다.", reason); //1000ms후에 rejected됩니다. error
-    //보통은 이처럼 js에서 사용하는 에러 객체를 매개변수로 넘기는게 일반적.
-  });
+Promise.race([p(3000), p(2000), p(1000)]).then((message) => {
+  console.log(
+    "가장 빠른애만 fulfilled된 이후에 실행되는 문장 입니다.",
+    message
+  ); //가장 빠른애만 fulfilled된 이후에 실행되는 문장 입니다. 1000
+});
