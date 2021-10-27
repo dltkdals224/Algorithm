50af0e1b124ac5ef6332893a9df65a0f65518bdb
Sangmin <23mean@naver.com>
2021-10-28 04:08:52 +0900

    2021.10.28 // 1단계 모두 완료
    1단계 모두 완료

--- Programmers/Level1/minimum_rectangle.js
+++ Programmers/Level1/minimum_rectangle.js
@@ -1,108 +1,25 @@
 //2021.10.11
+//2021.10.28 retry
 function solution(sizes) {
   let ans = 0;
   let tmp = 0;
-  let changeAns1 = 9999;
-  let changeAns2 = 9999;
-
-  let widthTmp = [];
-  let heightTmp = [];
 
   for (let n = 0; n < sizes.length; n++) {
-    widthTmp.push(sizes[n][0]);
-    heightTmp.push(sizes[n][1]);
-  }
-
-  // console.log(widthTmp);
-  // console.log(heightTmp);
-
-  function changeIndex1() {
-    let widthMax = Math.max(...widthTmp);
-    let heightMax = Math.max(...heightTmp);
-    ans = widthMax * heightMax;
-    let widthMaxIndex = [];
-    let heightMaxIndex = [];
-
-    widthTmp.filter((elem, index) => {
-      if (elem == widthMax) {
-        widthMaxIndex.push(index);
-      }
-    });
-    heightTmp.filter((elem, index) => {
-      if (elem == heightMax) {
-        heightMaxIndex.push(index);
-      }
-    });
-
-    for (let n = 0; n < widthMaxIndex.length; n++) {
-      tmp = widthTmp[widthMaxIndex[n]];
-      widthTmp.splice(widthMaxIndex[n], 1, heightTmp[n]);
-      heightTmp.splice(widthMaxIndex[n], 1, tmp);
-    }
-    // console.log(widthTmp);
-    // console.log(heightTmp);
-    changeAns1 = Math.max(...widthTmp) * Math.max(...heightTmp);
-    if (changeAns1 < ans) {
-      changeIndex1();
-    } else {
-      widthTmp = [];
-      heightTmp = [];
-
-      for (let n = 0; n < sizes.length; n++) {
-        widthTmp.push(sizes[n][0]);
-        heightTmp.push(sizes[n][1]);
-      }
+    if (sizes[n][0] < sizes[n][1]) {
+      tmp = sizes[n][0];
+      sizes[n][0] = sizes[n][1];
+      sizes[n][1] = tmp;
     }
   }
 
-  //
-
-  function changeIndex2() {
-    let widthMax = Math.max(...widthTmp);
-    let heightMax = Math.max(...heightTmp);
-    ans = widthMax * heightMax;
-    let widthMaxIndex = [];
-    let heightMaxIndex = [];
-
-    widthTmp.filter((elem, index) => {
-      if (elem == widthMax) {
-        widthMaxIndex.push(index);
-      }
-    });
-    heightTmp.filter((elem, index) => {
-      if (elem == heightMax) {
-        heightMaxIndex.push(index);
-      }
-    });
-
-    for (let n = 0; n < heightMaxIndex.length; n++) {
-      tmp = heightTmp[heightMaxIndex[n]];
-      heightTmp.splice(heightMaxIndex[n], 1, widthTmp[n]);
-      widthTmp.splice(heightMaxIndex[n], 1, tmp);
-    }
-    // console.log(widthTmp);
-    // console.log(heightTmp);
-    changeAns2 = Math.max(...widthTmp) * Math.max(...heightTmp);
-    if (changeAns2 < ans) {
-      changeIndex2();
-    } else {
-      widthTmp = [];
-      heightTmp = [];
-
-      for (let n = 0; n < sizes.length; n++) {
-        widthTmp.push(sizes[n][0]);
-        heightTmp.push(sizes[n][1]);
-      }
-    }
+  let longSideTmp = [];
+  let shortSideTmp = [];
+  for (let n = 0; n < sizes.length; n++) {
+    longSideTmp.push(sizes[n][0]);
+    shortSideTmp.push(sizes[n][1]);
   }
+  ans = Math.max(...longSideTmp) * Math.max(...shortSideTmp);
 
-  changeIndex1();
-  changeIndex2();
-  //3개중 최솟값이 답
-  console.log(ans);
-  console.log(changeAns1);
-  console.log(changeAns2);
-  ans = Math.min(ans, changeAns1, changeAns2);
   console.log(ans);
   return ans;
 }
