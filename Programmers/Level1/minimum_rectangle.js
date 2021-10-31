47e218621034f229378cc7855483adaab874b28d
Sangmin <23mean@naver.com>
2021-11-01 04:03:50 +0900

    2021.11.01
    값 수정

--- Programmers/Level1/minimum_rectangle.js
+++ Programmers/Level1/minimum_rectangle.js
@@ -23,19 +23,3 @@ function solution(sizes) {
   console.log(ans);
   return ans;
 }
-
-solution([
-  [10, 7],
-  [12, 3],
-  [8, 15],
-  [14, 7],
-  [5, 15],
-]);
-
-solution([
-  [14, 4],
-  [19, 6],
-  [6, 16],
-  [18, 7],
-  [7, 11],
-]);
