d802668cc1549dfb6f711647d7da386e4beff6f5
Sangmin <23mean@naver.com>
2021-11-02 22:04:09 +0900

    2021.11.02
    tmp

--- Programmers/test.js
+++ Programmers/test.js
@@ -1,15 +1,6 @@
-let s = "3people Unfollowed Me";
-console.log(s[0]);
+let arr = [];
+arr = "3People unFollowed me".split("");
+console.log(arr);
 
-if (isNaN(s[0])) console.log("11");
-if (isNaN(s[1])) console.log("22");
-
-console.log(String.fromCharCode(s[1].charCodeAt(0) - 32));
-console.log(typeof String.fromCharCode(s[1].charCodeAt(0) - 32));
-console.log(typeof Number(String.fromCharCode(s[1].charCodeAt(0) - 32)));
-
-let test = String.fromCharCode(s[1].charCodeAt(0) - 32);
-if (isNaN(test)) console.log("int형 아닙니다.");
-
-let test2 = Number(String.fromCharCode(s[1].charCodeAt(0) - 32));
-if (isNaN(test2)) console.log("int형 아닙니다.");
+arr.splice(0, 1, "T");
+console.log(arr);
