dc82328d0440fe8afb129ff5ff0298c1413e5da9
Sangmin <23mean@naver.com>
2021-11-27 15:12:26 +0900

    //2021.11.27
    피보나치 마지막 테스트 13,14 불통

--- Programmers/Level2/피보나치수.js
+++ Programmers/Level2/피보나치수.js
@@ -1,16 +1,28 @@
 //2021.11.02
+//2021.11.27 재도전
+// (A+B) % C === ((A % C) + (B % C)) % C를 알아야 풀 수 있다.
+// 자세한 사항은 '모듈러 연산의 성질'을 검색해보자.
 function solution(n) {
   let ans = -1;
 
-  function Fibonacci(n) {
-    if (n == 0) return 0;
-    else if (n == 1) return 1;
-    else return Fibonacci(n - 1) + Fibonacci(n - 2);
-  }
-  //30분정도 추가 고민해 보았지만 딱히 보이지 않음
-
-  ans = Fibonacci(n) % 1234567;
+  ans = Fibonacci(n);
   console.log(ans);
 
   return ans;
 }
+
+function Fibonacci(n, lastlast = 0, last = 1) {
+  if (n == 0) {
+    return lastlast;
+  } else if (n == 1) {
+    return last;
+  } else {
+    return Fibonacci(
+      n - 1,
+      last % 1234567,
+      ((lastlast % 1234567) + (last % 1234566)) % 1234567
+    );
+  }
+}
+
+// 아직 해결이 안됨
