feaf7a2ed5ab8e0829f412b226f7e4670f12ba5d
Sangmin <23mean@naver.com>
2021-11-29 01:05:18 +0900

    2021.11.29

--- Programmers/test.js
+++ Programmers/test.js
@@ -1,18 +1,1 @@
-// String객체 생성하기
-var strObj = new String("hello");
-console.log(strObj); //[String: 'hello']
-
-// Array(배열)객체 생성하기
-var strObj = new Array([1, 2, 3]);
-console.log(arrObj); //[ [ 1, 2, 3 ] ]
-
-acc = async function (a, b) {
-  delay(10);
-
-  if (a < 0) throw new Error("not supported");
-  return a + b;
-};
-let string = await add(-2, 4);
-console.log(string);
-// strObj
-// string
+console.log(Math.SQRT3);
