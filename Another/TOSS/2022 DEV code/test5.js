e1648dc5ae593ed3fad40a2b9e5e53fde636ef3b
Sangmin <23mean@naver.com>
2022-08-16 23:49:39 +0900

    2022.08.16 toss dev test5

--- Another/TOSS/2022 DEV code/test5.js
+++ Another/TOSS/2022 DEV code/test5.js
@@ -1,7 +1,26 @@
 const exclude = ",";
 
 function solution(text, keyword) {
+  const POSSIBILITY = keyword.length + 1;
   let html = "";
+
+  for (let i = 0; i < POSSIBILITY; i++) {
+    let TARGET_KEYWORD = "";
+    if (i === 0 || i === POSSIBILITY - 1) {
+      TARGET_KEYWORD = keyword;
+    } else {
+      TARGET_KEYWORD = keyword.slice(0, i) + "," + keyword.slice(i);
+    }
+
+    const regex = new RegExp(`${TARGET_KEYWORD}`);
+
+    if (text.includes(TARGET_KEYWORD)) {
+      html = text.replace(regex, `<mark>${TARGET_KEYWORD}</mark>`);
+      break;
+    }
+  }
+
+  console.log(html);
   return html;
 }
 
@@ -9,4 +28,7 @@ solution("커피 3,500원", "350");
 // '커피 <mark>3,50</mark>0원'
 
 solution("샌드위치 2,350원", "350");
-// '샌드위치 2,<mark>350</mark>원'
+// // '샌드위치 2,<mark>350</mark>원'
+
+solution("핫도그 350,000원", "350");
+// // '샌드위치 2,<mark>350</mark>원'
