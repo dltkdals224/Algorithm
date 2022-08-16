a03da8359710d653613072f7a296d0941b317c8e
Sangmin <23mean@naver.com>
2022-08-16 23:49:15 +0900

    2022.08.16 toss dev test3

--- Another/TOSS/2022 DEV code/test3.js
+++ Another/TOSS/2022 DEV code/test3.js
@@ -1,5 +1,26 @@
 function solution(paths) {
-  var answer = "";
+  let answer = "";
+
+  paths.map((v) => {
+    if (v === "..") {
+      answer = answer.slice(1, answer.lastIndexOf("/"));
+    }
+    if (v === "...") {
+      answer = answer.slice(1, answer.lastIndexOf("/"));
+      answer = answer.slice(1, answer.lastIndexOf("/"));
+    }
+
+    if (v !== ".." && v !== "...") {
+      answer += `/${v}`;
+    }
+  });
+
+  answer = answer.replace(/\/\//g, "/");
+  answer = answer.replace(/\/\./g, "");
+  if (answer[0] !== "/") {
+    answer = "/".concat(answer);
+  }
+
   return answer;
 }
 
