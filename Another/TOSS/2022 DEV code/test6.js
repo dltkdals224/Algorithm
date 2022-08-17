1024b61189854223a3c95f8ec5c7d46c3fcb258f
Sangmin <23mean@naver.com>
2022-08-17 19:48:54 +0900

    2022.08.16 toss dev test6

--- Another/TOSS/2022 DEV code/test6.js
+++ Another/TOSS/2022 DEV code/test6.js
@@ -1,7 +1,48 @@
 function solution(pin) {
-  var answer = true;
+  let answer = true;
+  const PIN_ARR = pin.split("");
+
+  // 4자리 모두 동일
+  if (allEqualNumber(PIN_ARR)) {
+    return false;
+  }
+
+  // 연속된 숫자 탐지
+  if (consecutiveNumber(PIN_ARR)) {
+    answer = false;
+  }
+
+  console.log(answer);
   return answer;
 }
+
+const allEqualNumber = (arr) => {
+  return arr.every((v) => v === arr[0]);
+};
+
+const consecutiveNumber = (arr) => {
+  ARR_LENGTH = arr.length;
+
+  for (let i = 0; i < ARR_LENGTH - 2; i++) {
+    if (
+      // 내림
+      (Number(arr[i]) - Number(arr[i + 1]) === 1 &&
+        Number(arr[i + 1]) - Number(arr[i + 2]) === 1) ||
+      (Number(arr[i]) - Number(arr[i + 1]) === -9 &&
+        Number(arr[i + 1]) - Number(arr[i + 2]) === 1) ||
+      // 오름
+      (Number(arr[i]) - Number(arr[i + 1]) === -1 &&
+        Number(arr[i + 1]) - Number(arr[i + 2]) === -1) ||
+      (Number(arr[i]) - Number(arr[i + 1]) === -1 &&
+        Number(arr[i + 1]) - Number(arr[i + 2]) === 9)
+    ) {
+      console.log(arr);
+      return true;
+    }
+  }
+
+  return false;
+};
 
 // 비유효
 // 4자리 모두 동일
@@ -22,5 +63,5 @@ solution("0129") === false;
 // 유효
 solution("1398") === true;
 solution("1478") === true;
-solution("1098") === true;
+solution("1097") === true;
 solution("9013") === true;
