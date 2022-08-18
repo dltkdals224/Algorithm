987585670085eb02cf1d18bb3a20c08443131a07
Sangmin <23mean@naver.com>
2022-08-18 19:11:50 +0900

    2022.08.16 toss dev test8

--- Another/TOSS/2022 DEV code/test8.js
+++ Another/TOSS/2022 DEV code/test8.js
@@ -1,10 +1,83 @@
 function parse(route, path) {
-  /* 작성해주세요 */
-
-  return {
-    matches: false,
+  const returnObj = {
+    matches: true,
+    params: {},
   };
+
+  const ROUTE_DEPTH = route.match(/\//g).filter((item) => item !== "").length;
+  let PROCESSED_PATH = path;
+
+  if (!PROCESSED_PATH.startsWith("/")) {
+    PROCESSED_PATH = "/" + PROCESSED_PATH;
+  }
+
+  for (let i = 0; i < ROUTE_DEPTH; i++) {
+    // route의 []값 판별, path의 number 판별
+    if (
+      route
+        .slice(findSlashPoint(route)[i] + 1, findSlashPoint(route)[i + 1])
+        .startsWith("[") &&
+      route
+        .slice(findSlashPoint(route)[i] + 1, findSlashPoint(route)[i + 1])
+        .endsWith("]") &&
+      !isNaN(
+        Number(
+          PROCESSED_PATH.slice(
+            findSlashPoint(PROCESSED_PATH)[i] + 1,
+            findSlashPoint(PROCESSED_PATH)[i + 1]
+          )
+        )
+      )
+    ) {
+      // matches:true에서 리턴할 객체 리턴 준비
+      const PARAMS_LENGTH = route.slice(
+        findSlashPoint(route)[i] + 1,
+        findSlashPoint(route)[i + 1]
+      ).length;
+
+      returnObj.params[
+        route
+          .slice(findSlashPoint(route)[i] + 1, findSlashPoint(route)[i + 1])
+          .slice(1, PARAMS_LENGTH - 1)
+      ] = PROCESSED_PATH.slice(
+        findSlashPoint(PROCESSED_PATH)[i] + 1,
+        findSlashPoint(PROCESSED_PATH)[i + 1]
+      );
+
+      continue;
+    }
+
+    // matches:false 걸러버리기
+    if (
+      route.slice(findSlashPoint(route)[i], findSlashPoint(route)[i + 1]) !==
+      PROCESSED_PATH.slice(
+        findSlashPoint(PROCESSED_PATH)[i],
+        findSlashPoint(PROCESSED_PATH)[i + 1]
+      )
+    ) {
+      return {
+        matches: false,
+      };
+    }
+  }
+
+  return returnObj;
 }
+
+const findSlashPoint = (path) => {
+  let pos = 0;
+  let returnArr = [];
+
+  while (true) {
+    let foundPos = path.indexOf("/", pos);
+    if (foundPos == -1) break;
+
+    returnArr.push(foundPos);
+    pos = foundPos + 1;
+  }
+
+  return returnArr;
+};
 
 function solution(route, path) {
   var result = parse(route, path);
