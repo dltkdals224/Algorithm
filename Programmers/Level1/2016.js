128998da8bf9aa6f6b3b931740295c6a04a00fb4
Sangmin <23mean@naver.com>
2022-09-05 23:17:49 +0900

    fix: 풀이 수정

--- Programmers/Level1/2016.js
+++ Programmers/Level1/2016.js
@@ -1,16 +1,27 @@
 //2021.09.15
 function solution(a, b) {
-    let someDay = new Date(2016, a - 1, b);
+  let someDay = new Date(2016, a - 1, b);
 
-    let ans = someDay.getDay();
+  let ans = someDay.getDay();
 
-    return (ans == 0) ? 'SUN' : (ans == 1) ? 'MON' : (ans == 2) ? 'TUE' : (ans == 3) ? 'WED' : (ans == 4) ? 'THU' : (ans == 5) ? 'FRI' : 'SAT';
+  return ans == 0
+    ? "SUN"
+    : ans == 1
+    ? "MON"
+    : ans == 2
+    ? "TUE"
+    : ans == 3
+    ? "WED"
+    : ans == 4
+    ? "THU"
+    : ans == 5
+    ? "FRI"
+    : "SAT";
 }
-
 
 //New Information
 //Date의 MDN을 자세히 봐볼것. 유용하게 사용 할 수 있음.
-//3항 연산자는 
+//3항 연산자는
 /*
 function example(…) {
     return condition1 ? value1
@@ -27,7 +38,6 @@ function example(…) {
 }
 꼴로 사용 할 수 있다
 */
-
 
 //Date()객체를 쓰는 행위가 알고리즘 문제 풀이의 본질을 해친다는 댓글을 보았다. 일리가 없지는 않아.
 //여태까지의 풀이가 헛된건 아닌듯.
