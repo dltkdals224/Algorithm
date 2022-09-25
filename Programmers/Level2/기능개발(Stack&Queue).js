cd9b312c0c12209daa83aec1f901481c477f58a8
Sangmin <23mean@naver.com>
2022-09-25 19:17:28 +0900

    fix: 풀이 수정

--- Programmers/Level2/기능개발(Stack&Queue).js
+++ Programmers/Level2/기능개발(Stack&Queue).js
@@ -2,7 +2,6 @@
 function solution(progresses, speeds) {
   let answer = [];
   let dayRequired = [];
-
   progresses.map((progress, index) => {
     dayRequired.push(Math.ceil((100 - progress) / speeds[index]));
   });
@@ -42,3 +41,21 @@ function solution(progresses, speeds) {
 
   return answer;
 }
+
+// 다른 사람의 풀이
+// function solution(progresses, speeds) {
+//   let answer = [0];
+//   let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+//   let maxDay = days[0];
+
+//   for(let i = 0, j = 0; i< days.length; i++){
+//       if(days[i] <= maxDay) {
+//           answer[j] += 1;
+//       } else {
+//           maxDay = days[i];
+//           answer[++j] = 1;
+//       }
+//   }
+
+//   return answer;
+// }
