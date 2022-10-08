8677c114a47d398c02c589b981422281864bbd60
Sangmin <23mean@naver.com>
2022-10-08 12:10:45 +0900

    2022.10.08 땅따먹기

--- Programmers/Level2/땅따먹기.js
+++ Programmers/Level2/땅따먹기.js
@@ -1,37 +1,37 @@
-//2021.11.13
-//전부 실패
+// 2022.10.08
 function solution(land) {
-  let ans = 0;
-  let len = land.length;
-  let top = [];
+  cumulativeLand = [...land];
 
-  for (let n = 0; n < len; n++) {
-    console.log(land[n]);
+  cumulativeLand.map((row, index) => {
+    if (index !== 0) {
+      let targetArr = [];
 
-    if (
-      land[n].indexOf(Math.max(...land[n])) ===
-      land[n - 1].indexOf(Math.max(...land[n - 1]))
-    ) {
-      //이제 hardcore한 비교연산이 필요하다.
-    } else {
-      top.push(Math.max(...land[n]));
+      for (let i = 0; i < row.length; i++) {
+        let tmp = cumulativeLand[index - 1][i];
+        cumulativeLand[index - 1][i] = -1;
+        targetArr.push(row[i] + Math.max(...cumulativeLand[index - 1]));
+        cumulativeLand[index - 1][i] = tmp;
+      }
+
+      cumulativeLand[index] = targetArr;
     }
-  }
-  console.log(top);
+  });
 
-  console.log(ans);
-  return ans;
+  return Math.max(...cumulativeLand[cumulativeLand.length - 1]);
 }
 
-solution([
-  [1, 2, 3, 5],
-  [5, 6, 7, 8],
-  [4, 3, 2, 1],
-]); // 16
-solution([
-  [1, 2, 3, 40],
-  [90, 95, 85, 100],
-  [1, 70, 60, 3],
-  [20, 50, 30, 1],
-  [1, 100, 2, 3],
-]); // 4행까지는 40,90,60,50 // 5행 가면서 40,90,70,30,100
+// solution([
+//   [1, 2, 3, 5],
+//   [5, 6, 7, 8],
+//   [4, 3, 2, 1],
+// ]); // 16
+
+// solution([
+//   [1, 2, 3, 5],
+//   [5, 6, 7, 100],
+//   [4, 3, 2, 1],
+// ]); // 107
+
+// 후기
+// 누적합에 대한 혁신적인 접근을 알게 됨.
+// 이전[i-1] 행에 대하여 -1 처리를 해놓고 최대합을 찾아 누적시켜 나아가는 방식.
