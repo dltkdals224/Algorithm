1972a7be6722f6df361b58d08ef0653d112d4e3a
Sangmin <23mean@naver.com>
2022-12-17 23:30:02 +0900

    2022.12.17 햄버거 만들기

---
+++ Programmers/Level1/햄버거 만들기.js
@@ -0,0 +1,28 @@
+function solution(ingredient) {
+  var answer = 0;
+
+  let i = 0;
+  let latest = [0];
+
+  while (i < ingredient.length - 3) {
+    if (
+      ingredient[i] == 1 &&
+      ingredient[i + 1] == 2 &&
+      ingredient[i + 2] == 3 &&
+      ingredient[i + 3] == 1
+    ) {
+      answer++;
+      ingredient.splice(i, 4);
+
+      i = latest.pop();
+    } else if (ingredient[i] == 1) {
+      latest.push(i);
+      i++;
+    } else i++;
+  }
+
+  return answer;
+}
+
+solution([2, 1, 1, 2, 3, 1, 2, 3, 1]); // 2
+solution([1, 3, 2, 1, 2, 1, 3, 1, 2]); // 0
