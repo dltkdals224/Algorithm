6391a7e83c93906a3acade44e0b1dd02dc7d93ed
Sangmin <23mean@naver.com>
2023-02-12 17:58:20 +0900

    2023.02.11 카카오_쿼드압축 후 개수 세기 2차 : test case 실패

--- Programmers/Level2/카카오_쿼드압축 후 개수 세기.js
+++ Programmers/Level2/카카오_쿼드압축 후 개수 세기.js
@@ -1,79 +1,66 @@
 // test 6~10,13~16 시간 초과
+// test 1~2, 5~8, 14~16 실패
 function solution(arr) {
   let tmpObj = {
     0: 0,
     1: 0,
   };
-  arr.forEach((row) => {
-    row.forEach((value) => {
-      tmpObj[value]++;
-    });
-  });
 
   const ARR_LENGTH = arr.length;
-  const COMPRESSION_STATUS_TYPE0 = new Map();
-  const COMPRESSION_STATUS_TYPE1 = new Map();
-
   for (let t = 2; t <= ARR_LENGTH; t *= 2) {
     for (let i = 0; i < ARR_LENGTH; i += t) {
       for (let j = 0; j < ARR_LENGTH; j += t) {
         const TARGET_VALUE = arr[i][j];
 
-        if (t === 2) {
-          if (
-            TARGET_VALUE === arr[i][j + 1] &&
-            TARGET_VALUE === arr[i + 1][j] &&
-            TARGET_VALUE === arr[i + 1][j + 1]
-          ) {
-            if (TARGET_VALUE === 0) {
-              COMPRESSION_STATUS_TYPE0.set(t, [
-                ...(COMPRESSION_STATUS_TYPE0.get(t) || []),
-                `${i}${j}`,
-              ]);
-            }
-            if (TARGET_VALUE === 1) {
-              COMPRESSION_STATUS_TYPE1.set(t, [
-                ...(COMPRESSION_STATUS_TYPE1.get(t) || []),
-                `${i}${j}`,
-              ]);
-            }
-            tmpObj[TARGET_VALUE] -= 3;
-          }
-        } else {
-          if (
-            TARGET_VALUE === 0 &&
-            COMPRESSION_STATUS_TYPE0.get(t / 2)?.includes(`${i}${j}`) &&
-            COMPRESSION_STATUS_TYPE0.get(t / 2)?.includes(`${i}${j + t / 2}`) &&
-            COMPRESSION_STATUS_TYPE0.get(t / 2)?.includes(`${i + t / 2}${j}`) &&
-            COMPRESSION_STATUS_TYPE0.get(t / 2)?.includes(
-              `${i + t / 2}${j + t / 2}`
-            )
-          ) {
-            COMPRESSION_STATUS_TYPE0.set(t, [
-              ...(COMPRESSION_STATUS_TYPE0.get(t) || []),
-              `${i}${j}`,
-            ]);
-            tmpObj[TARGET_VALUE] -= 3;
-          }
-          if (
-            TARGET_VALUE === 1 &&
-            COMPRESSION_STATUS_TYPE1.get(t / 2)?.includes(`${i}${j}`) &&
-            COMPRESSION_STATUS_TYPE1.get(t / 2)?.includes(`${i}${j + t / 2}`) &&
-            COMPRESSION_STATUS_TYPE1.get(t / 2)?.includes(`${i + t / 2}${j}`) &&
-            COMPRESSION_STATUS_TYPE1.get(t / 2)?.includes(
-              `${i + t / 2}${j + t / 2}`
-            )
-          ) {
-            COMPRESSION_STATUS_TYPE1.set(t, [
-              ...(COMPRESSION_STATUS_TYPE1.get(t) || []),
-              `${i}${j}`,
-            ]);
-            tmpObj[TARGET_VALUE] -= 3;
-          }
+        if (
+          typeof TARGET_VALUE == "number" &&
+          TARGET_VALUE === arr[i][j + t / 2] &&
+          TARGET_VALUE === arr[i + t / 2][j] &&
+          TARGET_VALUE === arr[i + t / 2][j + t / 2]
+        ) {
+          arr[i][j + t / 2] = -1;
+          arr[i + t / 2][j] = -1;
+          arr[i + t / 2][j + t / 2] = -1;
+          arr[i][j] = [TARGET_VALUE, t];
+        }
+
+        if (
+          typeof TARGET_VALUE != "number" &&
+          TARGET_VALUE[0] === arr[i][j + t / 2][0] &&
+          TARGET_VALUE[0] === arr[i + t / 2][j][0] &&
+          TARGET_VALUE[0] === arr[i + t / 2][j + t / 2][0] &&
+          TARGET_VALUE[1] === arr[i][j + t / 2][1] &&
+          TARGET_VALUE[1] === arr[i + t / 2][j][1] &&
+          TARGET_VALUE[1] === arr[i + t / 2][j + t / 2][1]
+        ) {
+          arr[i][j + t / 2] = -1;
+          arr[i + t / 2][j] = -1;
+          arr[i + t / 2][j + t / 2] = -1;
+          arr[i][j] = [TARGET_VALUE[0], t];
         }
       }
     }
   }
+
+  arr.forEach((row) => {
+    row.forEach((target) => {
+      if (typeof target == "number") {
+        if (target === 0) {
+          tmpObj[0]++;
+        }
+        if (target === 1) {
+          tmpObj[1]++;
+        }
+      } else {
+        if (target[0] === 0) {
+          tmpObj[0]++;
+        }
+        if (target[0] === 1) {
+          tmpObj[1]++;
+        }
+      }
+    });
+  });
 
   return [tmpObj[0], tmpObj[1]];
 }
@@ -95,3 +82,29 @@ solution([
   [0, 0, 0, 0, 1, 0, 0, 1],
   [0, 0, 0, 0, 1, 1, 1, 1],
 ]); // [10,15]
+
+solution([
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+]); // [10, 12]
+
+solution([
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+]); // [0, 1]
