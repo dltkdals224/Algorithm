af72cd9e4f4a2041027238afa7d627c8f224c27a
Sangmin <23mean@naver.com>
2023-03-24 17:34:50 +0900

    2023.03.24 [MEDIUM] Hash Table

---
+++ Leetcode/Hash Table/1282. Group the People Given the Group Size They Belong To.py.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        ans = []
+        tmp = []
+        targetList = (sorted(list(enumerate(groupSizes)), key=lambda x: x[1]))
+        for target in targetList:
+            tmp.append(target[0])
+            if(len(tmp) == target[1]):
+                ans.append(tmp)
+                tmp = []
+        return ans
