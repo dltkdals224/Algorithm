5eb2f2f3aa4f8ab211b6f26efd96a38a08a07a24
Sangmin <23mean@naver.com>
2024-12-17 15:52:04 +0900

    fix: 이전 javascript 풀이 제거

--- Programmers/Level2/카카오_메뉴 리뉴얼.js
+++
@@ -1,8 +0,0 @@
-function solution(orders, course) {
-  var answer = [];
-  return answer;
-}
-
-solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]); // ["AC", "ACDE", "BCFG", "CDE"]
-solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]); // ["ACD", "AD", "ADE", "CD", "XYZ"]
-solution(["XYZ", "XWY", "WXA"], [2, 3, 4]); // ["WX", "XY"]
